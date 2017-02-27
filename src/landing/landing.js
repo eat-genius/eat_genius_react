62d6a7fe0a358a8c56c34df525b2074994d83739
Christopher Metz <cmetz7@gmail.com>
2017-02-27 15:46:20 -0800

    worked on file structure

--- src/landing/landing.js
+++ src/landing/landing.js
@@ -5,26 +5,26 @@ import './landing.css';
 class Landing extends React.Component {
   render() {
     return (
-      <div class="landing">
+      <div className={"landing"}>
         <header>
-          <div class="header-text">
-            Can't Decide what to eat?
+          <div className={"header-text"}>
+            <div>Can't Decide what to eat?</div>
             <Link to="signup">Start Choosing Now!</Link>
           </div>
         </header>
         <main>
-            <div class="taglines">
-              <div class="icon"><h2><i class="material-icons">group</i></h2></div>
+            <div className={"taglines"}>
+              <div className={"icon"}><h2><i className={"material-icons"}>group</i></h2></div>
               <h5>Great For Groups!</h5>
             </div>
 
-            <div class="taglines">
-              <div class="icon"><h2><i class="material-icons">mood</i></h2></div>
+            <div className={"taglines"}>
+              <div className={"icon"}><h2><i className={"material-icons"}>mood</i></h2></div>
               <h5>Quickly Agree On What To Eat!</h5>
             </div>
 
-            <div class="taglines">
-              <div class="icon"><h2><i class="material-icons">restaurant</i></h2></div>
+            <div className={"taglines"}>
+              <div className={"icon"}><h2><i className={"material-icons"}>restaurant</i></h2></div>
               <h5>All The Restaurants In Yelp!</h5>
             </div>
           </main>
---
+++ src/signup/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './signup.css';
+
+class SignUp extends React.Component {
+  render() {
+    return (
+      <div>test</div>
+    )
+  }
+}
+
+module.exports = SignUp;
