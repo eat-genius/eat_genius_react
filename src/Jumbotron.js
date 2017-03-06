82d1a1ec38a946733d59e3907bfa872aa62f0a5f
Michael Friedman <contact@michaeldavidfriedman.com>
2017-03-06 15:59:10 -0800

    fixed jumbotron spacing.

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -14,7 +14,10 @@ class JumbotronComponent extends Component {
     return (
       <Jumbotron style={{ backgroundImage: this.state.backgroundImage }}>
         <h1 style={{ display: 'flex', justifyContent: 'center' }}>{this.state.title}</h1>
-        <div className='container'><p>{this.state.tagline}</p>                       <p><Button bsStyle='primary'>Learn more</Button></p></div>
+        <div className='container'>
+          <p>{this.state.tagline}</p>
+          <p><Button bsStyle='primary'>Learn more</Button></p>
+        </div>
       </Jumbotron>
     )
   }
